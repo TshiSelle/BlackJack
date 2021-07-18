48f45bf57f56a47d30c300e5fd114383cd4d8e9d
Omar <omar.houssami20@gmail.com>
2021-07-18 11:15:29 +0300

    scripting rdits

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 //BlackJack
-alert('sell')
-document.getElementById("bj-hit").addEventListener('click', bjHit)
+document.getElementById("bj-hit").addEventListener("click", bjHit)
+
 
 function bjHit() {
   alert('bee3a')
