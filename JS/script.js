df37034411769e1193d07e7fd01853e99b67e45f
OmarHoussami <omar.houssami20@gmail.com>
2021-07-20 16:25:00 +0300

    random function

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 let game = {
   'player': { 'scoreSpan': '#player-result', 'div': '#player-box', 'score': 0 },
   'cpu': { 'scoreSpan': '#dealer-result', 'div': '#dealer-box', 'score': 0 },
+  'cards': ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
 }
 const PLAYER = game['player']
 const CPU = game['cpu']
@@ -10,7 +11,8 @@ document.querySelector('#btn-hit').addEventListener("click", bjHit)
 document.querySelector('#btn-deal').addEventListener("click", dealBtn)
 
 function bjHit() {
-  showCard(PLAYER);
+  showCard(PLAYER)
+  showCard(CPU);
 }
 function showCard(activePlayer) {
   let cardImage = document.createElement('img')
@@ -20,7 +22,18 @@ function showCard(activePlayer) {
 }
 function dealBtn() {
   let playerImages = document.querySelector('#player-box').querySelectorAll('img')
-  for (let i = 0; i < playerImages.length; i++) {
+  let cpuImages = document.querySelector('#dealer-box').querySelectorAll('img')
+
+
+  for (let i = 0; i < playerImages.length; i++) { // player array removal
     playerImages[i].remove();
   }
+  for (let i = 0; i < cpuImages.length; i++) { //dealer array removal
+    cpuImages[i].remove();
+  }
+
+}
+function randomCard() {
+  let randomIndex = Math.floor(Math.random() * 13)
+  return bjHit['cards'][randomIndex]
 }
