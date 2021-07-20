e1e75323bcad3586eecbde69fe29e35383971e74
Omar <omar.houssami20@gmail.com>
2021-07-20 15:26:56 +0300

    created cpu and player objects

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,10 @@
+let game = {
+  'player': { 'scoreSpan': '#player-result', 'div': '#player-box', 'score': 0 },
+  'cpu': { 'scoreSpan': '#dealer-result', 'div': '#dealer-box', 'score': 0 },
+}
+const PLAYER = game['player']
+const CPU = game['cpu']
+
 document.querySelector('#btn-hit').addEventListener("click", bjHit)
 
 function bjHit() {
